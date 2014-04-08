614501cfcb1560c6830ff46a981e0928b41e25fd
Michael Feihstel <michaelfeihstel@gmail.com>
2014-04-08 11:46:31 +0200

    asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ LungeDe::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Compress JavaScripts and CSS
