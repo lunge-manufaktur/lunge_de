1d59d244aa30c4f6e853b0f58b56b8d21c0542f6
Michael Feihstel <michaelfeihstel@gmail.com>
2014-04-16 12:24:33 +0200

    set compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ LungeDe::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Add Modernizr to asset pipeline
   config.assets.precompile += %w( vendor/modernizr.js )
