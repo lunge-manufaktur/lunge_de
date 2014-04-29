ebdfc0871874d76454bf6bf6f78eaf4d7233040f
Michael Feihstel <michaelfeihstel@gmail.com>
2014-04-29 08:42:16 +0200

    fixed dummy migration

--- db/migrate/20140326135552_dummy_migration.rb
+++ db/migrate/20140326135552_dummy_migration.rb
@@ -1,9 +1,9 @@
-class AddAttachmentImageToPosts < ActiveRecord::Migration
+class DummyMigration < ActiveRecord::Migration
   def self.up
-    
+    # nothing
   end
 
   def self.down
-    
+    # nothing
   end
 end
