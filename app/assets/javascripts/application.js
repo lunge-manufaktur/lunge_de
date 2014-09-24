2d71edaa894e27e2e20b4e28e4e6bef23a043d76
Michael Feihstel <michaelfeihstel@gmail.com>
2014-09-24 14:18:00 +0200

    move GA tracking code to head

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,12 +21,3 @@ $(document).foundation();
 
 // Hide notices after a few seconds
 $('.notice').slideDown(500).delay(5000).slideUp(500);
-
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-4191408-5', 'auto');
-ga('send', 'pageview');
