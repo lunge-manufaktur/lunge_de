9bf5e9e75bb7d6adb00f79a556dccd81fcf3a7ea
Michael Feihstel <michaelfeihstel@gmail.com>
2014-10-01 12:10:57 +0200

    use correct image path

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -43,7 +43,7 @@ class Brand < ActiveRecord::Base
   	:default_url => "/images/:style/missing.png",
   	:storage => :ftp,
   	:path => "/lunge.de/www/images/:class/:name/:name_:id_:style.:extension",
-  	:url => "http://www.lunge.de/images/:class/:name/:name_:id_:style.:extension",
+  	:url => "http://cdn.lunge.de/images/:class/:name/:name_:id_:style.:extension",
   	:ftp_servers => [
       {
         :host     => "www.lunge.de",
