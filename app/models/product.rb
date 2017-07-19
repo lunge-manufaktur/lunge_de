b1f5b622f145c1d5eadf486a793aa1948f345583
Michael Feihstel <michaelfeihstel@gmail.com>
2017-07-19 10:14:58 +0200

    unpublish out of stock products

--- app/models/product.rb
+++ app/models/product.rb
@@ -55,6 +55,9 @@ class Product < ActiveRecord::Base
   scope :has_image, -> { includes(:product_images).where.not(product_images: { id: nil } ) }
   scope :prefer_with_image, -> { includes(:product_images).order('product_images.created_at DESC NULLS LAST') }
 
+  # callbacks
+  after_validation :hide_unavailable_product, on: [:create, :update]
+
   # methods
   include IconHelper
 
@@ -119,15 +122,44 @@ class Product < ActiveRecord::Base
 
   def has_stock?(store: nil, size: nil)
     product_id = id
-    store_id = store || "*"
-    size = size
+    store_id = store || [1,3,4,5]
+    size = size ||  "COALESCE(g1, 0)
+                    +COALESCE(g1h, 0)
+                    +COALESCE(g2, 0)
+                    +COALESCE(g2h, 0)
+                    +COALESCE(g3, 0)
+                    +COALESCE(g3h, 0)
+                    +COALESCE(g4, 0)
+                    +COALESCE(g4h, 0)
+                    +COALESCE(g5, 0)
+                    +COALESCE(g5h, 0)
+                    +COALESCE(g6, 0)
+                    +COALESCE(g6h, 0)
+                    +COALESCE(g7, 0)
+                    +COALESCE(g7h, 0)
+                    +COALESCE(g8, 0)
+                    +COALESCE(g8h, 0)
+                    +COALESCE(g9, 0)
+                    +COALESCE(g9h, 0)
+                    +COALESCE(g10, 0)
+                    +COALESCE(g10h, 0)
+                    +COALESCE(g11, 0)
+                    +COALESCE(g11h, 0)
+                    +COALESCE(g12, 0)
+                    +COALESCE(g12h, 0)
+                    +COALESCE(g13, 0)
+                    +COALESCE(g13h, 0)
+                    +COALESCE(g14, 0)
+                    +COALESCE(g14h, 0)
+                    +COALESCE(g15, 0)
+                    +COALESCE(g16, 0)
+                    +COALESCE(g17, 0)
+                    +COALESCE(g18, 0)
+                    +COALESCE(g19, 0)"
 
-    stock = Stock.where(store_id: store_id, product_id: product_id).first
-    if size
-      eval("stock.#{size}?")
-    else
-      stock.quantity > 0
-    end
+    stock = Stock.where(store_id: store_id, product_id: product_id).sum(size)
+    
+    stock > 0
   end
 
   def eligible_for_running_course?
@@ -148,3 +180,10 @@ class Product < ActiveRecord::Base
     end
   end
 end
+
+
+
+private
+  def hide_unavailable_product
+    self.is_published = false unless has_stock?
+  end
