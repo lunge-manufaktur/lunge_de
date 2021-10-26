f843dd8e588ee412a72ede519eb60d0dd2470c27
Michael Feihstel <michaelfeihstel@gmail.com>
2021-10-26 14:44:19 +0200

    change site root to products_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
 
   # sitemap
   get "sitemap", controller: "sitemaps", action: "show", format: "xml"
